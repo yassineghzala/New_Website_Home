a5610d003a86e5c0993a35aca514cf1e8892597e
yassineghzala <ghzalayassin@gmail.com>
2023-08-24 18:58:54 +0100

    trochiket

--- src/app/components/admin/fix/navbar/dashboard.component.spec.ts
+++ src/app/components/admin/fix/navbar/dashboard.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { DashboardComponent } from './dashboard.component';
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
+      declarations: [DashboardComponent]
     });
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
